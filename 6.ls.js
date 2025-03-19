3c943489bb1ed6e74c5a288173c672f227c35a77
JuanjoRestrepo <restrepojuanjo@gmail.com>
2025-03-19 17:59:52 -0500

    ls directory

--- 6.ls.js
+++ 6.ls.js
@@ -1,13 +1,16 @@
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 
-fs.readdir('.', (err, files) => {
-  if (err) {
-    console.error('Error al leer el directorio: ', err);
-    return;
-  }
-
-  console.log('\n==== Archivos ====');
-  files.forEach((file) => {
-    console.log(file);
+// ===== USANDO CATCH =====
+fs.readdir('.')
+  .then((files) => {
+    console.log('\n==== Archivos ====');
+    files.forEach((file) => {
+      console.log(file);
+    });
+  })
+  .catch((err) => {
+    if (err) {
+      console.error('Error al leer el directorio: ', err);
+      return;
+    }
   });
-});
---
+++ 7.process.js
@@ -0,0 +1,17 @@
+// argumentos de entrada
+//console.log(process.argv);
+
+// controlar el proceso y su salida
+// process.exit(0);  // 1 -> error
+// process.exit(1);  // 0 -> exito
+
+// poodemos controlar eventos del proceso
+// process.on('exit', (code) => {
+//   // limpiar los recursos
+// });
+
+// current working directory
+console.log(process.cwd());
+
+// platform
+console.log(process.env.PEPITO);
