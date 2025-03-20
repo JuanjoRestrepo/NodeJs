20e407f7024c01e3f4e4ae46d84b64c33e356a21
JuanjoRestrepo <restrepojuanjo@gmail.com>
2025-03-19 22:06:48 -0500

    Cleared Concepts

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -1,12 +1,24 @@
 const http = require('node:http'); // Protocolo HTTP
+const fs = require('node:fs');
 
 const desiredPort = process.env.PORT ?? 1234;
 
 // Servidor
 const processRequest = (req, res) => {
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
+
   if (req.url === '/') {
-    res.end('<h1>Bienvenido a mi página de inicio</h1>');
+    res.end('<h1>Mi página</h1>');
+  } else if (req.url === '/imagen-super-bonita.jpg') {
+    fs.readFile('./placa.jpg', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('<h1>500 Internal Server Error</h1>');
+      } else {
+        res.setHeader('Content-Type', 'image/jpg');
+        res.end(data);
+      }
+    });
   } else if (req.url === '/contacto') {
     res.end('<h1>Contacto</h1>');
   } else {
