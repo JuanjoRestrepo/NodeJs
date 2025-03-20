f77fecc8da3492b8eff2188964fdd443a4fed8b8
JuanjoRestrepo <restrepojuanjo@gmail.com>
2025-03-19 21:25:37 -0500

    Status Codes

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -4,17 +4,12 @@ const desiredPort = process.env.PORT ?? 1234;
 
 // Servidor
 const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
   if (req.url === '/') {
-    res.statusCode = 200; // OK
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.end('<h1>Bienvenido a mi página de inicio</h1>');
   } else if (req.url === '/contacto') {
-    res.statusCode = 200; // OK
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.end('<h1>Contacto</h1>');
   } else {
-    res.statusCode = 404; // Not Found
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.end('<h1>404 Not Found</h1>');
   }
 };
