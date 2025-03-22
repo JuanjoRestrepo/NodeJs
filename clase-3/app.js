8dc8ac98fb474947324e918935efe45482c258d7
Juan Jose Restrepo Rosero <restrepojuanjo@gmail.com>
2025-03-21 22:20:29 -0500

    Added  proper validations

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,6 +1,8 @@
 const express = require('express'); // usamos require luego pasaremos a commonJS
 const crypto = require('node:crypto'); // modulo de node para hashear
 const movies = require('./movies.json');
+const { error } = require('node:console');
+const { validateMovie } = require('./schemas/movies');
 
 const app = express();
 app.use(express.json()); // middleware que parsea el body de la request a JSON
@@ -35,17 +37,16 @@ app.get('/movies/:id', (req, res) => {
 });
 
 app.post('/movies', (req, res) => {
-  const { title, genre, year, director, duration, rate, poster } = req.body;
+  const result = validateMovie(req.body);
+  if (result.error) {
+    // 422 Unprocessable Entity
+    // 400 Bad Request. Error propio del cliente. La solicitud contiene sintaxis incorrecta y no debería repetirse.
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
 
   const newMovie = {
     id: crypto.randomUUID(), // uuid v4
-    title,
-    genre,
-    director,
-    year,
-    duration,
-    rate: rate ?? 0,
-    poster,
+    ...result.data,
   };
   // Esto no seria REST, porque estamos guardando
   // el estado de la aplicacion en memoria
