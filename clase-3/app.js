ee709cd26db9d657d707fef6b77729f647870210
Juan Jose Restrepo Rosero <restrepojuanjo@gmail.com>
2025-03-21 19:06:31 -0500

    Retriving movies.json with API Request

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,10 +1,16 @@
 const express = require('express'); // usamos require luego pasaremos a commonJS
+const movies = require('./movies.json');
 
 const app = express();
 app.disable('x-powered-by'); // deshabilitamos la cabecera X-Powered-By: Express
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
+});
+
+// Todos los recursos que sean MOVIES se identifican con  /movies
+app.get('/movies', (req, res) => {
+  res.json(movies);
 });
 
 const PORT = process.env.PORT ?? 1234; // si PORT no esta definido en el entorno, usamos 1234
