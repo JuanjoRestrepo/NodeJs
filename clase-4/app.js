07d6c5ca286afb2428cd054bc76d09002e84bb6d
Juan Jose Restrepo Rosero <restrepojuanjo@gmail.com>
2025-03-29 15:33:42 -0500

    created Middleware with cors.js and cors function to return Cors Functionability

--- clase-4/app.js
+++ clase-4/app.js
@@ -1,6 +1,6 @@
 import express, { json } from 'express'; // usamos require luego pasaremos a commonJS
-import cors from 'cors';
 import { moviesRouter } from './routes/movies.js';
+import { corsMiddleware } from './middlewares/cors.js';
 
 // EN EL FUTURO: el import del json sería así:
 //import movies from './movies.json' assert { type: 'json' };
@@ -11,35 +11,8 @@ import { moviesRouter } from './routes/movies.js';
 
 const app = express();
 app.use(json()); // middleware que parsea el body de la request a JSON
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      const ACCEPTED_ORIGINS = [
-        'http://localhost:8080',
-        'http://localhost:1234',
-        'https://movies.com',
-        'https://midu.dev',
-      ];
-
-      if (ACCEPTED_ORIGINS.includes(origin)) {
-        return callback(null, true);
-      }
-
-      if (!origin) {
-        return callback(null, true);
-      }
-
-      return callback(new Error('Not allowed by CORS'));
-    },
-  })
-);
+app.use(corsMiddleware());
 app.disable('x-powered-by'); // deshabilitar el header X-Powered-By: Express
-
-// métodos normales: GET/HEAD/POST
-// métodos complejos: PUT/PATCH/DELETE
-
-// CORS PRE-Flight
-// OPTIONS
 
 // Todos los recursos que sean MOVIES se identifica con /movies
 app.use('/movies', moviesRouter);
@@ -49,3 +22,9 @@ const PORT = process.env.PORT ?? 1234;
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`);
 });
+
+// métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
